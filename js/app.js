2556dab504ad9fd5dfcb4099c2eb9e2f0e317028
tijani abdellatif <tijani.idrissi.abdellatif@gmail.com>
2021-10-24 20:15:24 +0200

    starter

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const API_KEY = '';
+const endPoint = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key='+API_KEY+'&page=1';
+const imgUrl = 'https://image.tmdb.org/t/p/w1280';
+const searchUrl ='https://api.themoviedb.org/3/search/movie?api_key='+API_KEY+'&query="';
